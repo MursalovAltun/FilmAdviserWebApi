4f9cc22334362e073156ead57d9ce5632a650cc5
AltunMursalov <deliri0us1357@gmail.com>
2020-03-03 14:20:03 +0400

    Init

---
+++ Common.WebApiCore/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Common.WebApiCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "FilmAdviserWebApi";
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Common.WebApiCore/Setup/CentralizedPrefixConvention.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Common.WebApiCore.Setup
+{
+    public class CentralizedPrefixConvention : IApplicationModelConvention
+    {
+        private readonly AttributeRouteModel _centralizedPrefix;
+
+        public CentralizedPrefixConvention(IRouteTemplateProvider routeTemplateProvider)
+        {
+            this._centralizedPrefix = new AttributeRouteModel(routeTemplateProvider);
+        }
+
+        public void Apply(ApplicationModel app)
+        {
+            foreach (var controller in app.Controllers)
+            {
+                var matchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel != null).ToList();
+
+                foreach (var selectorModel in matchedSelectors)
+                {
+                    selectorModel.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(this._centralizedPrefix, selectorModel.AttributeRouteModel);
+                }
+
+                var unmatchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel == null).ToList();
+                foreach (var selectorModel in unmatchedSelectors)
+                {
+                    selectorModel.AttributeRouteModel = this._centralizedPrefix;
+                }
+            }
+        }
+    }
+}
